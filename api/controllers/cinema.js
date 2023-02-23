8d02c4c164d7d8414703d011015e19bc79fd00a1
sbeknur2003@gmail.com <sbeknur2003@gmail.com>
2023-02-23 11:44:15 +0600

    .

--- api/controllers/cinema.js
+++ api/controllers/cinema.js
@@ -1,53 +1,84 @@
-import Movie from "../models/Movie.js";
+import Cinema from "../models/Cinema.js";
 
-export const createMovie = async (req, res, next) => {
-    const newMovie = new Movie(req.body);
+export const createCinema = async (req, res, next) => {
+    const newCinema = new Cinema(req.body);
 
     try {
-        const savedMovie = await newMovie.save();
-        res.status(200).json(savedMovie);
+        const savedCinema = await newCinema.save();
+        res.status(200).json(savedCinema);
     } catch (err) {
         next(err);
     }
 };
 
-export const updateMovie = async (req, res, next) => {
+export const updateCinema = async (req, res, next) => {
     try {
-        const updatedMovie = await Movie.findByIdAndUpdate(
+        const updatedCinema = await Cinema.findByIdAndUpdate(
             req.params.id,
             {
                 $set: req.body,
             },
             { new: true }
         );
-        res.status(200).json(updatedMovie);
+        res.status(200).json(updatedCinema);
     } catch (err) {
         next(err);
     }
 };
 
-export const deleteMovie = async (req, res, next) => {
+export const deleteCinema = async (req, res, next) => {
     try {
-        await Movie.findByIdAndDelete(req.params.id);
-        res.status(200).json("Movie has been deleted");
+        await Cinema.findByIdAndDelete(req.params.id);
+        res.status(200).json("Cinema has been deleted");
     } catch (err) {
         next(err);
     }
 };
 
-export const getMovie = async (req, res, next) => {
+export const getCinema = async (req, res, next) => {
     try {
-        const movie = await Movie.findById(req.params.id);
-        res.status(200).json(movie);
+        const cinema = await Cinema.findById(req.params.id);
+        res.status(200).json(cinema);
     } catch (err) {
         next(err);
     }
 };
 
-export const getMovies = async (req, res, next) => {
+export const getCinemas = async (req, res, next) => {
     try {
-        const movies = await Movie.find(req.params.id).limit(req.query.limit);
-        res.status(200).json(movies);
+        const cinemas = await Cinema.find(req.params.id);
+        res.status(200).json(cinemas);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const countByCity = async (req, res, next) => {
+    const cities = req.query.cities.split(",");
+    try {
+        const list = await Promise.all(
+            cities.map((city) => {
+                return Cinema.countDocuments({ city: city });
+            })
+        );
+        res.status(200).json(list);
+    } catch (err) {
+        next(err);
+    }
+};
+export const countByType = async (req, res, next) => {
+    try {
+        const chaplinCount = await Cinema.countDocuments({ type: "chaplin" });
+        const kinoparkCount = await Cinema.countDocuments({ type: "kinopark" });
+        const kinoplexCount = await Cinema.countDocuments({ type: "kinoplex" });
+        const armanCount = await Cinema.countDocuments({ type: "arman" });
+
+        res.status(200).json([
+            { type: "chaplins", count: chaplinCount },
+            { type: "kinoparks", count: kinoparkCount },
+            { type: "kinoplex's", count: kinoplexCount },
+            { type: "armans", count: armanCount },
+        ]);
     } catch (err) {
         next(err);
     }
